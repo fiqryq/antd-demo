63e623881a21f6b63fcd33949054bca87046254b
fiqrychoerudin <fiqrychoerudin48@gmail.com>
2019-11-27 13:53:19 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { DatePicker, message } from 'antd';
+import 'antd/dist/antd.css';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+  state = {
+    date: null,
+  };
+
+  handleChange = date => {
+    message.info(`Selected Date: ${date ? date.format('YYYY-MM-DD') : 'None'}`);
+    this.setState({ date });
+  };
+
+  render() {
+    const { date } = this.state;
+    return (
+      <div style={{ width: 400, margin: '100px auto' }}>
+        <DatePicker onChange={this.handleChange} />
+        <div style={{ marginTop: 20 }}>
+          Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
